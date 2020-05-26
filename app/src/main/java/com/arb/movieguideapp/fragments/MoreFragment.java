b93ad33a9eb7b84f2d2c46d2f26d694b52dfdf18
Arb Tahiri <63010713+arbbitahiri@users.noreply.github.com>
2020-05-26 22:16:57 +0200

    Login nga Firebase

--- app/src/main/java/com/arb/movieguideapp/fragments/MoreFragment.java
+++ app/src/main/java/com/arb/movieguideapp/fragments/MoreFragment.java
@@ -1,17 +1,26 @@
 package com.arb.movieguideapp.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.arb.movieguideapp.LoginActivity;
+import com.arb.movieguideapp.MainActivity;
 import com.arb.movieguideapp.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MoreFragment extends Fragment {
+
+    private TextView txtSignOut;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Nullable
     @Override
@@ -22,5 +31,15 @@ public class MoreFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        txtSignOut = view.findViewById(R.id.txt_sign_out);
+
+        txtSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+            }
+        });
     }
 }
